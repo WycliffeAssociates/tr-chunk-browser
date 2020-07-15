ad9e9606e04d3ee7d0a1020ac95fcd8916ad0ded
mxaln <murmax82@gmail.com>
2020-07-15 07:35:53 -0400

    Integrated launch4j plugin

--- src/main/kotlin/com/matthewrussell/trchunkbrowser/app/main.kt
+++ src/main/kotlin/com/matthewrussell/trchunkbrowser/app/main.kt
@@ -1,10 +1,19 @@
 package com.matthewrussell.trchunkbrowser.app
 
 import com.matthewrussell.trchunkbrowser.app.mainview.MainView
+import javafx.scene.image.Image
+import javafx.stage.Stage
 import tornadofx.App
 import tornadofx.launch
 
-class MainApp : App(MainView::class)
+class MainApp : App(MainView::class) {
+    override fun start(stage: Stage) {
+        stage.icons.add(
+            Image(javaClass.getResource("/launcher.png").openStream())
+        )
+        super.start(stage)
+    }
+}
 
 fun main(args: Array<String>) {
     launch<MainApp>()
