c583670d1ae08ae7e2c9557a17c84efd8d069cda
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-04-06 11:00:07 -0400

    refactor directory merge

--- common/src/main/kotlin/org/bibletranslationtools/trchunkbrowser/common/domain/DirectoryMerge.kt
+++ common/src/main/kotlin/org/bibletranslationtools/trchunkbrowser/common/domain/DirectoryMerge.kt
@@ -4,23 +4,28 @@ import io.reactivex.Completable
 import org.bibletranslationtools.trchunkbrowser.common.model.AudioSegment
 import java.io.File
 
-class DirectoryMerge() {
+class DirectoryMerge {
     fun merge(inputDir: File, outputDir: File? = null): Completable {
         val mergeOutput = outputDir
             ?: inputDir.parentFile.resolve("${inputDir.name}-out").apply { mkdirs() }
+
         return Completable.fromCallable {
-            val segments = mutableListOf<AudioSegment>()
-            inputDir.walk().filter { it.isDirectory }.forEach { dir ->
-                val filesInDir = dir.listFiles(File::isFile)
-                if (filesInDir.any()) {
-                    segments.clear()
-                    filesInDir.forEach {
-                        segments.addAll(GetWavSegments(it).segments().blockingGet())
-                    }
-                    ExportSegments(segments)
-                        .exportMerged(mergeOutput)
-                        .subscribe()
+            mergeSubDirRecursively(inputDir, mergeOutput)
+        }
+    }
+
+    private fun mergeSubDirRecursively(root: File, outputDir: File) {
+        val segments = mutableListOf<AudioSegment>()
+        root.walk().filter { it.isDirectory }.forEach { dir ->
+            val filesInDir = dir.listFiles(File::isFile)
+            if (filesInDir.any()) {
+                segments.clear()
+                filesInDir.forEach {
+                    segments.addAll(GetWavSegments(it).segments().blockingGet())
                 }
+                ExportSegments(segments)
+                    .exportMerged(outputDir)
+                    .subscribe()
             }
         }
     }
